83f90ece155496818a57784dd45009894bbccde1
darwish <289582304@qq.com>
2025-10-23 20:12:12 +0800

    chore: 重构项目文件

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -2,7 +2,7 @@ import FetchClient from "../fetch-client";
 import type { LoginApiParams, User } from "./model/user.type";
 
 
-export const LoginApi: Api.IFetch<User, LoginApiParams> = (data) => FetchClient.post("/user/account/login", { data });
+export const LoginApi: Api.IFetch<User, LoginApiParams> = (data) => FetchClient.post("/api/user/login", { data });
 
 /** 获取微信openid */
 // export const WechatOpenIdApi: Api.IFetch<WechatOpenIdResponse, WechatOpenIdParams> = (params) => ApiService.post("/realms/soxen-saas/protocol/openid-connect/token", {
